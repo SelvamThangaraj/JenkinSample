cca5ce4db6fe784d0f5b5aa71dc4bccdd15faf65
SelvamThangaraj <44888490+SelvamThangaraj@users.noreply.github.com>
2018-12-11 20:07:17 -0600

    Update MorningItem.java

--- src/MorningItem.java
+++ src/MorningItem.java
@@ -2,7 +2,7 @@
 public class MorningItem {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Jenkins  ");
+		System.out.println("Welcome to Jenkins ........edited for jenkin build ");
 
 	}
 
