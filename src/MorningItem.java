6c24d9b346482555c56cdeff9dfb3fad389d2d25
selvamthangaraj <spark_sel@yahoo.co.in>
2018-12-11 19:42:37 -0600

    first commit

---
+++ src/MorningItem.java
@@ -0,0 +1,9 @@
+
+public class MorningItem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Jenkins");
+
+	}
+
+}
---
+++ src/SecondJob.java
@@ -0,0 +1,9 @@
+
+public class SecondJob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
